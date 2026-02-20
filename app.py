cf2e1754b36156a96ba161fa1f0e520295b90e83
Woodstock <emil.moreno2002@gmail.com>
2026-02-20 15:06:59 -0600

    Adapted html files refs to work with Flask

--- app.py
+++ app.py
@@ -7,6 +7,46 @@ app = Flask(__name__)
 def index_get():
     return render_template("index.html")
 
+@app.get("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.get("/blog")
+def blog():
+    return render_template("blog.html")
+
+
+@app.get("/blog-details")
+def blog_details():
+    return render_template("blog-details.html")
+
+
+@app.get("/shop")
+def shop():
+    return render_template("shop.html")
+
+
+@app.get("/shop-details")
+def shop_details():
+    return render_template("shop-details.html")
+
+
+@app.get("/cart")
+def shopping_cart():
+    return render_template("shopping-cart.html")
+
+
+@app.get("/checkout")
+def checkout():
+    return render_template("checkout.html")
+
+
+@app.get("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 @app.post("/predict")
 def predict():
     text = request.get_json().get("message")
